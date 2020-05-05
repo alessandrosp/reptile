5358a526cc8e3244166ba03d58dc7c246a1aa513
Alessandro Scoccia Pappagallo <alessandrosp@alessandrosp-macbookpro2.roam.corp.google.com>
2020-05-05 23:08:10 +0900

    Modified setup before deployment

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name="reptile", version="1.0", packages=find_packages())
+setuptools.setup(
+    name="reptile",
+    version="1.0",
+    author="Alessandro",
+    url="https://github.com/alessandrosp/reptile",
+    description=(
+        "Reptile is a command-line interface for Python. Specifically, "
+        "Reptile helps with producing interactive REPL-like software."
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=(
+        "prompt-toolkit, cli, command-line, commandline, "
+        "command-line-interface, python-inquiry, inquirer, "
+        "reptile, REPL, prompt"
+    ),
+    install_requires=["prompt-toolkit>=3.0.5", "pygments>=2.6.1"],
+    python_requires=">=3.6",
+)
