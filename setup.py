9003c99411e76b9a89462c9bb008b49a2cb8cff1
Alessandro Scoccia Pappagallo <alessandrosp@alessandrosp-macbookpro2.roam.corp.google.com>
2020-05-17 00:04:59 +0900

    Changed long_description and packages

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="reptile",
-    version="1.0",
+    version="1.0.2",
     author="Alessandro",
     url="https://github.com/alessandrosp/reptile",
     description=(
         "Reptile is a command-line interface for Python. Specifically, "
         "Reptile helps with producing interactive REPL-like software."
     ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -21,4 +26,5 @@ setuptools.setup(
     ),
     install_requires=["prompt-toolkit>=3.0.5", "pygments>=2.6.1"],
     python_requires=">=3.6",
+    packages=setuptools.find_packages(),
 )
